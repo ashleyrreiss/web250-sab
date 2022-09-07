f8f37cd11430abc55f1060449e0ee6fb206d6c23
Ashley Reiss <arreiss@gmail.com>
2022-09-07 17:47:11 -0400

    Completed third inheritance challenge

--- asgn02-inheritance/challenge-02-inheritance.php
+++ asgn02-inheritance/challenge-02-inheritance.php
@@ -1,28 +1,37 @@
 <?php 
 
 class Books {
-  var $title;
-  var $author;
+  public $title;
+  public $author;
+  public $format;
 
-  function book_by_author() {
+  public function book_by_author() {
     return $this->title . " is by " . $this->author . ".<br>";
+  }
+
+  public function format() {
+    return "Format: " . $this->format;
+  }
+
+  public function set_format($value) {
+    $this->format = $value;
   }
 }
 
 class Novel extends Books {
-  var $is_fiction = true;
-  var $main_character;
+  protected $is_fiction = true;
+  public $main_character;
 
-  function type() {
+  public function type() {
     return "This book is a fictional story about a character named " . $this->main_character . ".<br>";
   }
 }
 
 class Textbook extends Books {
-  var $is_fiction = false;
-  var $subject;
+  protected $is_fiction = false;
+  public $subject;
 
-  function type() {
+  public function type() {
     return "This book is a nonfiction book about " . $this->subject . ".<br>";
   }
 }
@@ -37,11 +46,15 @@ $b2->title = "A Web-Based Introduction to Programming";
 $b2->author = "Mike O'Kane";
 $b2->subject = "computer programming";
 
-echo $b1->book_by_author() . "<br>";
-echo $b1->type() . "<br>";
+echo $b1->book_by_author();
+echo $b1->type();
+$b1->set_format("Hardcover");
+echo $b1->format() . "<br><br>";
 
-echo $b2->book_by_author() . "<br>";
-echo $b2->type() . "<br>";
+echo $b2->book_by_author();
+echo $b2->type();
+$b2->set_format("Paperback");
+echo $b2->format() . "<br><br>";
 
 
 ?>

bf226fdc04a8a760a71081f37f1c941e35792805
Ashley Reiss <arreiss@gmail.com>
2022-10-05 19:44:19 -0400

    establish dev branch for asgn06

---
+++ asgn06/bird/private/db_credentials.php
@@ -0,0 +1,18 @@
+<?php
+
+// ----- Localhost
+
+define("DB_SERVER", "localhost");
+define("DB_USER", "webuser");
+define("DB_PASS", "birdDatabaseUser");
+define("DB_NAME", "bird");
+
+
+// ----- Siteground
+
+// define("DB_SERVER", "localhost");
+// define("DB_USER", "webuser");
+// define("DB_PASS", "birdDatabaseUser");
+// define("DB_NAME", "bird");
+
+?>

32939b8dc2fd3d01b9c6a3e8fe78b41ec8edd984
Ashley Reiss <arreiss@gmail.com>
2022-10-12 20:38:14 -0400

    finished preliminary work on asgn07

--- asgn07/bike/private/db_credentials.php
+++ asgn07/bike/private/db_credentials.php
@@ -2,17 +2,17 @@
 
 // Local host
 
-// define("DB_SERVER", "localhost");
-// define("DB_USER", "bikeuser");
-// define("DB_PASS", "bikeuserpw");
-// define("DB_NAME", "chain_gang");
+define("DB_SERVER", "localhost");
+define("DB_USER", "bikeuser");
+define("DB_PASS", "bikeuserpw");
+define("DB_NAME", "chain_gang");
 
 
 // Siteground
 
-define("DB_SERVER", "localhost");
-define("DB_USER", "umigmydpceum8");
-define("DB_PASS", "BikeUserPW");
-define("DB_NAME", "dbt9cwc3kucorz");
+// define("DB_SERVER", "localhost");
+// define("DB_USER", "umigmydpceum8");
+// define("DB_PASS", "BikeUserPW");
+// define("DB_NAME", "dbt9cwc3kucorz");
 
 ?>
